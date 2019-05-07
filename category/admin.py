c4c0aed3c68bec108a2adfd8c68b337dcc23f3e3
oneyan1 <yangrodno@gmail.com>
2019-05-07 10:24:20 +0200

    fully worked admin panel for category/produkt model

--- category/admin.py
+++ category/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Product
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug':('name',)}
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'category', 'price', 'quantity', 'available']
+    list_filter = ['available', 'createdDate', 'upgradeDate']
+    list_editable = ['price', 'quantity', 'available']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- category/models.py
+++ category/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=150, db_index=True)
-
+    slug = models.SlugField(max_length=150, unique=True, db_index=True)
+    
     class Meta:
         ordering = ['name']
 
@@ -13,17 +14,18 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(Category , related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=150, db_index=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField()
     available = models.BooleanField(default=True)
-    createdDate = models.DateTimeField(auto_now_add=True)
-    upgradeDate = models.DateTimeField(auto_now=True)
+    createdDate = models.DateTimeField(auto_now_add=True, verbose_name='Data dodania')
+    upgradeDate = models.DateTimeField(auto_now=True, verbose_name='Data aktualizacji')
     images = models.ImageField(upload_to='images/', blank='True', )
 
     class Meta:
         ordering = ['name']
-        index_together = ['id']
+        index_together = [('id', 'slug'),]
 
     def __str__(self):
         return self.name
