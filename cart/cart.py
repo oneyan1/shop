d85ef03fa99f41fe57600aae6177d4d3440fd6aa
oneyan1 <yangrodno@gmail.com>
2019-06-01 17:27:16 +0200

    cart fix

--- cart/cart.py
+++ cart/cart.py
@@ -9,10 +9,44 @@ class Cart(object):
         if not cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
-
+    
+    def save(self):
+        self.session[settings.CART_SESSION_ID] = self.cart
+        self.session.modified = True
+    
+    
     def add(self, product, quantity=1, update_quantity = False):
         product_id = str(product.id)
         if product_id not in self.cart:
            self.cart[product_id] = {'quantity':0,
                                     'price': str(product.price)}
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
 
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Produсt.objects.filter(id__in=product_ids)
+        for product in products:
+            self.cart[str(product.id)]['product'] = product
+        for item in self.cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def getTotalPrice(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.session.modified = True
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from category.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
 
-# Create your views here.
+@require_POST
+
+def CartAdd(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id= product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product = product, quantity = cd['quantity'],
+                update_quantity = cd['update'])
+    return redirect('cart:CartDetail')
+
+def CartRemove(request, product_id):4
