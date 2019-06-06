29917a5756dbb9ff866b5807af3caa3a774b728e
oneyan1 <yangrodno@gmail.com>
2019-06-06 23:27:03 +0200

    add mailer

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
@@ -14,7 +16,10 @@ def OrderCreate(request):
                                         price=item['price'],
                                         quantity=item['quantity'])
                 cart.clear()
+                send_mail('Nowe zamówienie', 'Nowe zamówienie dodane do admin panelu', 
+                        'sklepszkielety@gmail.com', ['yangrodno@yandex.ru'], fail_silently=False)
                 return render(request, 'orders/order/created.html', {'order':order})
     
     form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'form':form})
+
--- shop/settings.py
+++ shop/settings.py
@@ -128,3 +128,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sklepszkelety'
+EMAIL_HOST_PASSWORD = 'morkowka983'
